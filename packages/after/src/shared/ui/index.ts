c2ebbece684ec8b68050afaaa423082046476d8d
jinseoit <jinseoit@gmail.com>
2025-11-27 02:56:19 +0900

    modify : 미사용 컴포넌트 제거

--- packages/after/src/shared/ui/index.ts
+++ packages/after/src/shared/ui/index.ts
@@ -1,11 +1,10 @@
 // Custom UI components
-export { Button } from './Button';
-export { Alert } from './Alert';
-export { Badge } from './Badge';
-export { Modal } from './Modal';
-export { Header } from './Header';
-export { Table } from './Table';
-export { FormInput } from './FormInput';
-export { FormSelect } from './FormSelect';
-export { FormTextarea } from './FormTextarea';
-export { FormCheckbox } from './FormCheckbox';
+export { Button } from "./Button";
+export { Alert } from "./Alert";
+export { Badge } from "./Badge";
+export { Modal } from "./Modal";
+export { Header } from "./Header";
+export { Table } from "./Table";
+export { FormInput } from "./FormInput";
+export { FormSelect } from "./FormSelect";
+export { FormTextarea } from "./FormTextarea";
