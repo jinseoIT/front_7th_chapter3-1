217e8e20ce9cda8162f8b2cfa526fe5a3f7ba1e2
jinseoit <jinseoit@gmail.com>
2025-11-28 04:31:35 +0900

    build : 배포 로직 추가

--- packages/after/vite.config.ts
+++ packages/after/vite.config.ts
@@ -3,9 +3,11 @@ import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
+const base: string = process.env.NODE_ENV === "production" ? "/front_7th_chapter3-1/" : "";
 // https://vite.dev/config/
 export default mergeConfig(
   defineConfig({
+    base,
     plugins: [react()],
     resolve: {
       alias: {
